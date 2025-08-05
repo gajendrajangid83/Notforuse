3c43d35077905d4a7af01b37605290c75ee8d924
gajendrajangid83 <saraswatisharmapbc@gmail.com>
2025-08-05 12:32:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7804036800:AAEFKgbGjX2E11ZWmQtMbzZBJxrSMyYVQD0")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
 API_ID = int(os.environ.get("API_ID", "25025064"))
@@ -14,7 +14,7 @@ ADMINS = int(os.environ.get("ADMINS", "7995988644"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://saraswatisharmapbc:L8aRiWhBbhi8mFwN@cluster0.gxhxgye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "saraswati sharma")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
